924971f6b0dd7d3d431209ad82b1cb053c610313
pandi <bhishmduttsharma814@gmail.com>
2026-03-15 22:46:04 +0530

    logoutbtn

--- app/src/main/java/com/example/chattapp/MainActivity.java
+++ app/src/main/java/com/example/chattapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.chattapp;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -30,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     UserAdpter adapter;
     FirebaseDatabase database;
     ArrayList<Users> usersArrayList;
+    ImageView imglogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +62,36 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        imglogout = findViewById(R.id.logoutimg);
+
+        imglogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(MainActivity.this,R.style.dialoge);
+                dialog.setContentView(R.layout.dialog_layout);
+                Button no,yes;
+                yes = dialog.findViewById(R.id.yesbnt);
+                no = dialog.findViewById(R.id.nobnt);
+                yes.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        FirebaseAuth.getInstance().signOut();
+                        Intent intent = new Intent(MainActivity.this,login.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+                no.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+
 
             }
         });
